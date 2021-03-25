4818b9fc7ca5c34633eb8c3e3328d1d56143e19b
alexbmoreira <alexbmoreira@gmail.com>
2021-03-24 20:14:41 -0400

    Added components and views folders
    Each folder contains a sample component.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CharacterList from './views/CharacterList';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CharacterList />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/views/CharacterList.js
@@ -0,0 +1,12 @@
+import CharacterListItem from '../components/CharacterListItem'
+
+function CharacterList() {
+  return (
+    <div>
+      <h1>Character List</h1>
+      <CharacterListItem />
+    </div>
+  );
+}
+
+export default CharacterList;
