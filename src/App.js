f48917906242b6faa9fc002b9aec0810bf94c32d
alexbmoreira <alexbmoreira@gmail.com>
2021-03-25 22:01:10 -0400

    Moved things to an App component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CharacterList from './views/CharacterList';
+
+function App() {
+    return (
+        <div className="h-screen w-screen bg-gray-800 text-white">
+          <CharacterList />
+        </div>
+    );
+}
+  
+export default App;
