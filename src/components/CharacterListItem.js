c0ae3d3b3af78aafed2b02585cd2df45c0b522a8
alexbmoreira <alexbmoreira@gmail.com>
2021-03-25 22:48:55 -0400

    Converted components from functions to classes

--- src/components/CharacterListItem.js
+++ src/components/CharacterListItem.js
@@ -1,16 +1,20 @@
-function CharacterList(props) {
-    return (
-        <div className="flex my-2 justify-between">
-            <div className="flex flex-col">
-                <p className="text-xl font-bold">{props.name}</p>
-                <p className="text-sm">Born: {props.birth_year}</p>
+import { Component } from "react";
+
+class CharacterList extends Component {
+    render() {
+        return (
+            <div className="flex my-2 justify-between">
+                <div className="flex flex-col">
+                    <p className="text-xl font-bold">{this.props.name}</p>
+                    <p className="text-sm">Born: {this.props.birth_year}</p>
+                </div>
+                <div className="flex flex-col text-right">
+                    <p className="text-gray-300 text-sm">Height: {this.props.height}cm</p>
+                    <p className="text-gray-300 text-sm">Mass: {this.props.mass}kg</p>
+                </div>
             </div>
-            <div className="flex flex-col text-right">
-                <p className="text-gray-300 text-sm">Height: {props.height}cm</p>
-                <p className="text-gray-300 text-sm">Mass: {props.mass}kg</p>
-            </div>
-        </div>
-    );
+        );
+    }
 }
   
 export default CharacterList;
--- src/views/CharacterList.js
+++ src/views/CharacterList.js
@@ -1,18 +1,20 @@
 import CharacterListItem from '../components/CharacterListItem'
 
 import characters from '../api/characters'
+import { Component } from 'react';
 
-function CharacterList() {
-  const characterArray = characters.map((character, i) => {
-    return <CharacterListItem name={characters[i].name} birth_year={characters[i].birth_year} height={characters[i].height} mass={characters[i].mass} />
-  })
-
-  return (
-    <div className="mx-auto w-2/3">
-      <h1>Character List</h1>
-      {characterArray}
-    </div>
-  );
+class CharacterList extends Component {
+  render() {
+    const characterArray = characters.map((character, i) => {
+      return <CharacterListItem name={characters[i].name} birth_year={characters[i].birth_year} height={characters[i].height} mass={characters[i].mass} />
+    })
+    return (
+      <div className="mx-auto w-2/3">
+        <h1>Character List</h1>
+        {characterArray}
+      </div>
+    );
+  }
 }
 
 export default CharacterList;
