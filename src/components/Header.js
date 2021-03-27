737490e5d508a7731813ad983b4bd43881e495b4
alexbmoreira <alexbmoreira@gmail.com>
2021-03-26 21:00:41 -0400

    Moved header into its own component

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+      <div>
+        <h1 className="sw-font text-center text-5xl text-yellow-400">STAR WARS DB</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
