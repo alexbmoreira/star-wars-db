b583632d21125302b665dea0aa16ec1f73bedb71
alexbmoreira <alexbmoreira@gmail.com>
2021-03-26 20:04:37 -0400

    Fixed unknown height and mass

--- src/components/CharacterListItem.js
+++ src/components/CharacterListItem.js
@@ -8,6 +8,10 @@ class CharacterList extends Component {
         const swReg = /http:\/\/swapi.dev\/api\/people\/([0-9]+)/
         const swUrl = this.props.character.url
         const url = `/character/${swUrl.match(swReg)[1]}`
+
+        const heightUnits = this.props.character.height === "unknown" ? "" : "cm"
+        const massUnits = this.props.character.mass === "unknown" ? "" : "kg"
+
         return (
             <Link to={url}>
                 <div className="flex my-2 justify-between">
@@ -16,8 +20,8 @@ class CharacterList extends Component {
                         <p className="text-sm">Born: {this.props.character.birth_year}</p>
                     </div>
                     <div className="flex flex-col text-right">
-                        <p className="text-gray-300 text-sm">Height: {this.props.character.height}cm</p>
-                        <p className="text-gray-300 text-sm">Mass: {this.props.character.mass}kg</p>
+                        <p className="text-gray-300 text-sm">Height: {this.props.character.height}{heightUnits}</p>
+                        <p className="text-gray-300 text-sm">Mass: {this.props.character.mass}{massUnits}</p>
                     </div>
                 </div>
             </Link>
